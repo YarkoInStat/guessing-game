373f7be57e59f6b54fb1ad007378cfc0fca024a9
Iaroslav Gritsenko <yaroslav.gritsenko@instatsport.com>
2023-02-25 22:01:55 +0300

    done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guessDigit = Math.round((this.min + this.max) / 2)
+        return this.guessDigit
     }
 
     lower() {
-
+        this.max = this.guessDigit
     }
 
     greater() {
-
+        this.min = this.guessDigit
     }
 }
 
